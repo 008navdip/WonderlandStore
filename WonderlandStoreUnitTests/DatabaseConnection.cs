973804731480131c93f8a14de09b9d3fba69ac23
008navdip <66803065+008navdip@users.noreply.github.com>
2020-10-02 10:48:37 +1300

    Add files via upload

---
+++ WonderlandStoreUnitTests/DatabaseConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WonderlandStore;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DatabaseConnection
+    {
+        [TestMethod]
+        public void DatabaseConnection_IfOpen_ReturnsTrue()
+        {
+            WonderlandDatabaseOperations ms = new WonderlandDatabaseOperations();
+            bool v = ms.TestWonderlandDatabaseConnection();
+            Assert.IsTrue(v);
+        }
+    }
+}
